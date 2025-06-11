3de409aa01caaee9739c7cd3f21d2606f729868f
Altaskur <105789412+altaskur@users.noreply.github.com>
2025-06-11 22:37:31 +0200

    directo dia 11 (#6)

--- src/app/components/buttons/button-rectangular-big/button-rectangular-big.component.ts
+++ src/app/components/buttons/button-rectangular-big/button-rectangular-big.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {buttonColors} from "../../../interfaces/interfaces";
+import {CommonModule} from "@angular/common";
 
 @Component({
-  selector: 'app-button-rectangular-big',
-  imports: [],
-  templateUrl: './button-rectangular-big.component.html',
-  styleUrl: './button-rectangular-big.component.scss'
+    selector: 'app-button-rectangular-big',
+    imports: [CommonModule],
+    templateUrl: './button-rectangular-big.component.html',
+    styleUrl: './button-rectangular-big.component.scss'
 })
 export class ButtonRectangularBigComponent {
-
+    color = input.required<buttonColors>();
 }
